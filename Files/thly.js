8ace278e42cd6982d7c3057f482bebd26fcaa705
Hvftk <Hvftk@users.noreply.github.com>
2020-05-02 11:42:43 +0800

    增加通话录音

---
+++ Files/thly.js
@@ -0,0 +1,14 @@
+/*
+通话录音解锁vip--Alex0510(Mr.Eric)
+下载链接:https://itunes.apple.com/cn/app/id1502359606?mt=8&from=singlemessage&isappinstalled=0
+正则:http://micro-tool-api.foundao.com/orderPayCenterService/user/userInfo
+hostname=micro-tool-api.foundao.com
+quantumult X正则：
+http:\/\/micro-tool-api\.foundao\.com\/orderPayCenterService\/user\/userInfo url script-response-body thly.js
+
+
+*/
+let obj = JSON.parse($response.body);
+obj.data.isVip = 1,
+obj.data.vipExpireTime = "2970-01-01 00:00:00"
+$done({body: JSON.stringify(obj)});
