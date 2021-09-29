9dc0b1e8efb7f8fc93025570539bae31a1f1ff45
Hvftk <57172166+Hvftk@users.noreply.github.com>
2021-09-29 15:40:00 +0800

    Add files via upload

--- Files/ytb-ui-check.js
+++ Files/ytb-ui-check.js
@@ -71,35 +71,37 @@ function test() {
   return new Promise((resolve, reject) => {
     let option = {
       url: BASE_URL,
-      opts: opts,
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+        'Accept-Language': 'en',
       },
     }
-    $task.fetch(option).then (response => {
+    $task.fetch(option).then(response=> {
+      let data = response.body
       console.log(response.statusCode)
-      if (response.statusCode === 404) {
-        resolve('Not Found')
+      if (response.statusCode !== 200) {
+        reject('Error')
         return
       }
 
-      if (response.statusCode === 403) {
+      if (data.indexOf('premium is not available in your country') !== -1) {
         resolve('Not Available')
         return
       }
 
-      if (response.statusCode === 200) {
-        let url = response.headers['X-Originating-URL']
-        let region = url.split('/')[3]
-        region = region.split('-')[0]
-        if (region == 'title') {
-          region = 'us'
-        }
-        resolve(region)
-        return
+      let region = ''
+      let re = new RegExp('"countryCode":"(.*?)"', 'gm')
+      let result = re.exec(data)
+      if (result != null && result.length === 2) {
+        region = result[1]
+      } else if (data.indexOf('www.google.cn') !== -1) {
+        region = 'CN'
+      } else {
+        region = 'US'
       }
-      reject('Error')
+      resolve(region)
     })
   })
 }
+
