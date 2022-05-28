80579244a30360bd6c4ec370811f46681e0f6f3e
Hvftk <57172166+Hvftk@users.noreply.github.com>
2022-05-28 22:03:54 +0800

    Update zbk.js

--- css/zbk.js
+++ css/zbk.js
@@ -1,13 +1,13 @@
 /* 以下感谢Cuttlefish提供的灵感和帮助
 
-JS中直接CSS的模版：
+JS中直接使用CSS的模版：
 var body = $response.body.replace(/<head>/, '<head><style type="text/css">填写CSS格式内容</style>').replace(/'6572'/g, "'6578'");
 $done({ body });
 填写了一个CSS的实际案例：
 var body = $response.body.replace(/<head>/, '<head><style type="text/css">img[id^=ad]{display:none!important}</style>').replace(/'6572'/g, "'6578'");
 $done({ body });
 
-"填写CSS格式内容" 直接复制以下网址即可 https://cdn.jsdelivr.net/gh/Hvftk/Personal_JS@master/css/zbk.min.css
+"填写CSS格式内容" 直接复制以下网址呈现的CSS即可 https://cdn.jsdelivr.net/gh/Hvftk/Personal_JS@master/css/zbk.min.css
 
 */
 
